b11c1af11be73c7334241c4957bec82ff31b88dd
unknown <nxh16112005@gmail.com>
2026-04-27 16:13:31 +0700

    update

--- classification.py
+++ classification.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import accuracy_score, f1_score, classification_report
+from lazypredict.Supervised import LazyClassifier
+import pickle
 
 data = pd.read_csv(r"diabetes.csv")
 
@@ -16,16 +17,21 @@ x_train, x_test, y_train, y_test = train_test_split(   #tách bộ test là 20% 
     x, y, test_size = 0.2, random_state = 42
 )
 
-x_train, x_val, y_train, y_val = train_test_split(
-    x_train, y_train, test_size = 0.25, random_state = 42 #tách bộ validate 20% từ 80% dữ liệu còn lại là 0,25%
-)
-
-scaler = MinMaxScaler()
+scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test) #Bộ test không bao giờ được gọi fit
 
-model = RandomForestClassifier()
-model.fit(x_train, y_train)
+# params = {
+#     "n_estimators": [50, 100, 200],
+#     "criterion": ["gini", "entropy","log_loss"]
+# }
+#
+# model = GridSearchCV(RandomForestClassifier(random_state = 100), param_grid = params, scoring = "recall", cv=6, verbose = 2)
+# model.fit(x_train, y_train)
+# y_predict = model.predict(x_test)
+# print(model.best_params_)
+# print(model.best_score_)
+# print(classification_report(y_test, y_predict))
 
-y_predict = model.predict(x_test)
-print(classification_report(y_test, y_predict))
+clf = LazyClassifier(verbose = 0, ignore_warnings = True, custom_metric=None)
+models, predictions = clf.fit(x_train, x_test, y_train, y_test)
